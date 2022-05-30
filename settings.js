12d17ad75151a836e0eb7c0f0c31f1af90cad0be
Puling <80240315+Pulingz@users.noreply.github.com>
2022-05-30 06:54:59 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,22 +22,22 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281293036735','6281293036735','6281293036735'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281293036735'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281293036735' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Pulingz' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Pulingz security' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'pulingz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.ig = 'https://instagram.com/pulingzsec'
+global.mygc = 'https://chat.whatsapp.com/C70rRYkzvdoEPHGZaaXIdD'
 global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.email = 'afaiz6335@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
